34f36bbcdb510e7481fbf0e8d592e1ecb13b7179
Marc Khoury <marc.khoury@isae.edu.lb>
2019-04-14 06:30:42 -0700

    Add files via upload

--- tp4/question1/PatternObservateur.java
+++ tp4/question1/PatternObservateur.java
@@ -31,7 +31,14 @@ public class PatternObservateur extends junit.framework.TestCase {
         // bons paramètres
 
         // à compléter !!
-
+          assertEquals(l1, o1.senders().pop());
+        assertEquals(" 1 ", o1.arguments().pop());
+        assertEquals(l1, o2.senders().pop());
+        assertEquals(" 1 ", o2.arguments().pop());
+        assertEquals(l1, o1.senders().pop());
+        assertEquals("test", o1.arguments().pop());
+        assertEquals(l1, o2.senders().pop());
+        assertEquals("test", o2.arguments().pop());
         // ne pas modifier ces lignes, dernières assertions vraies de cette
         // méthode
         assertTrue(o1.senders().empty() && o1.arguments().empty());
@@ -55,7 +62,14 @@ public class PatternObservateur extends junit.framework.TestCase {
         // vérifier que l'observateur a bien été notifié par les deux listes
 
         // à compléter !!
-
+        assertEquals(l2, o.senders().pop());
+        assertEquals(" B ", o.arguments().pop());
+        assertEquals(l2, o.senders().pop());
+        assertEquals("testB", o.arguments().pop());
+        assertEquals(l1, o.senders().pop());
+        assertEquals(" A ", o.arguments().pop());
+        assertEquals(l1, o.senders().pop());
+        assertEquals("testA", o.arguments().pop());
         // ne pas modifier cette ligne, dernière assertion vraie de cette
         // méthode
         assertTrue(o.senders().empty() && o.arguments().empty());
@@ -77,7 +91,12 @@ public class PatternObservateur extends junit.framework.TestCase {
         // et deleteObservers()
 
         // à compléter !!
-
+           assertEquals(2, l1.countObservers());
+        assertEquals(2, l2.countObservers());
+        
+         l1.deleteObserver(o1);
+        l1.deleteObserver(o2);
+        l2.deleteObservers();
         // ne pas modifier ces lignes, dernières assertions vraies de cette
         // méthode
         assertTrue(o1.senders().empty());
